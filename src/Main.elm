9449cb1e3c072191692a23bb13c04df1f349810d
Yoshitaka Totsuka <y047aka@gmail.com>
2022-11-14 20:58:09 +0900

    feat: Implement timer

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,11 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (..)
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
 import Task
 import Time
+import Time.Extra as Time exposing (Interval(..))
 
 
 
@@ -27,12 +29,22 @@ main =
 type alias Model =
     { zone : Time.Zone
     , time : Time.Posix
+    , timer : Timer
     }
+
+
+type Timer
+    = Ready
+    | Counting { startedAt : Time.Posix, splitTime : Int }
+    | Stopped { startedAt : Time.Posix, stoppedAt : Time.Posix, splitTime : Int }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model Time.utc (Time.millisToPosix 0)
+    ( { zone = Time.utc
+      , time = Time.millisToPosix 0
+      , timer = Ready
+      }
     , Task.perform AdjustTimeZone Time.here
     )
 
@@ -44,20 +56,45 @@ init _ =
 type Msg
     = Tick Time.Posix
     | AdjustTimeZone Time.Zone
+    | Start
+    | Pause
+    | Reset
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Tick newTime ->
-            ( { model | time = newTime }
+            ( { model | time = newTime }, Cmd.none )
+
+        AdjustTimeZone newZone ->
+            ( { model | zone = newZone }, Cmd.none )
+
+        Start ->
+            ( case model.timer of
+                Ready ->
+                    { model | timer = Counting { startedAt = model.time, splitTime = 0 } }
+
+                Stopped { startedAt, stoppedAt, splitTime } ->
+                    { model | timer = Counting { startedAt = model.time, splitTime = splitTime + Time.diff Millisecond model.zone startedAt stoppedAt } }
+
+                _ ->
+                    model
             , Cmd.none
             )
 
-        AdjustTimeZone newZone ->
-            ( { model | zone = newZone }
+        Pause ->
+            ( case model.timer of
+                Counting { startedAt, splitTime } ->
+                    { model | timer = Stopped { startedAt = startedAt, stoppedAt = model.time, splitTime = splitTime } }
+
+                _ ->
+                    model
             , Cmd.none
             )
+
+        Reset ->
+            ( { model | timer = Ready }, Cmd.none )
 
 
 
@@ -65,8 +102,8 @@ update msg model =
 
 
 subscriptions : Model -> Sub Msg
-subscriptions model =
-    Time.every 1000 Tick
+subscriptions _ =
+    Time.every 200 Tick
 
 
 
@@ -75,14 +112,95 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
+    Html.node "body"
+        []
+        [ viewDigitalClock { zone = model.zone, time = model.time }
+        , viewTimer model
+        ]
+
+
+viewDigitalClock : { a | zone : Time.Zone, time : Time.Posix } -> Html Msg
+viewDigitalClock { zone, time } =
     let
         hour =
-            String.fromInt (Time.toHour model.zone model.time)
+            Time.toHour zone time
+                |> String.fromInt
+                |> String.padLeft 2 '0'
 
         minute =
-            String.fromInt (Time.toMinute model.zone model.time)
-
-        second =
-            String.fromInt (Time.toSecond model.zone model.time)
+            Time.toMinute zone time
+                |> String.fromInt
+                |> String.padLeft 2 '0'
     in
-    h1 [] [ text (hour ++ ":" ++ minute ++ ":" ++ second) ]
+    text (hour ++ ":" ++ minute)
+
+
+viewTimer : Model -> Html Msg
+viewTimer model =
+    div []
+        [ viewElapsedTime model
+        , viewTimerControl model
+        ]
+
+
+viewElapsedTime : Model -> Html Msg
+viewElapsedTime { zone, time, timer } =
+    div []
+        [ text <|
+            case timer of
+                Ready ->
+                    "00:00:00"
+
+                Counting { startedAt, splitTime } ->
+                    (splitTime + Time.diff Millisecond zone startedAt time) |> toDigitalString
+
+                Stopped { startedAt, stoppedAt, splitTime } ->
+                    (splitTime + Time.diff Millisecond zone startedAt stoppedAt) |> toDigitalString
+        ]
+
+
+toDigitalString : Int -> String
+toDigitalString time =
+    let
+        absolute =
+            abs time
+
+        sign =
+            if time < 0 then
+                "-"
+
+            else
+                ""
+
+        hours =
+            (absolute // 3600000)
+                |> String.fromInt
+                |> String.padLeft 2 '0'
+
+        minutes =
+            (modBy 3600000 absolute // 60000)
+                |> String.fromInt
+                |> String.padLeft 2 '0'
+
+        seconds =
+            (modBy 60000 absolute // 1000)
+                |> String.fromInt
+                |> String.padLeft 2 '0'
+    in
+    sign ++ String.join ":" [ hours, minutes, seconds ]
+
+
+viewTimerControl : Model -> Html Msg
+viewTimerControl model =
+    div [] <|
+        case model.timer of
+            Ready ->
+                [ button [ onClick Start ] [ text "Start" ] ]
+
+            Counting _ ->
+                [ button [ onClick Pause ] [ text "Pause" ] ]
+
+            Stopped _ ->
+                [ button [ onClick Start ] [ text "Resume" ]
+                , button [ onClick Reset ] [ text "Reset" ]
+                ]
